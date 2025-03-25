d57eca4349cf8484531f2a82245db5bf890782cd
Milos Milovanovic <53410962+MMilos6@users.noreply.github.com>
2025-03-25 03:14:49 +0100

    feat: disable landscape

--- app/layout.js
+++ app/layout.js
@@ -19,8 +19,9 @@ export default function RootLayout({ children }) {
             <head>
                 <meta name="theme-color" content="#323232" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="default" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
-                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
             </head>
             <body>
                 <ToastContainer
